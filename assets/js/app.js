a1368a71c02e7e028019d8752c775be3fe7889fb
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-18 03:58:55 +0600

    alert popup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,40 @@
 const popup = document.getElementById("popup");
-const clickBtn=document.getElementById('clickBtn')
+const clickBtn = document.getElementById("clickBtn");
 const confirmationDiv = document.getElementById("confirmationDiv");
 
 let result = false;
-const openPopup=()=> {
-
-  if(!result){
-      popup.classList.remove('hidden')
-      result=true
-  }else{
-      popup.classList.add('hidden')
-      result=false
+const openPopup = () => {
+  if (!result) {
+    popup.classList.remove("hidden");
+    result = true;
+  } else {
+    popup.classList.add("hidden");
+    result = false;
   }
-}
+};
 
-const cancelPopup=()=>{
-    popup.classList.add("hidden");
-    result=false
-}
+const cancelPopup = () => {
+  popup.classList.add("hidden");
+  result = false;
+};
 
-const deleteConfirmation=()=>{
-    confirmationDiv.classList.remove("hidden");
-}
+const deleteConfirmation = () => {
+  confirmationDiv.classList.remove("hidden");
+};
 
-const cancelConfirmation=()=>{
-    confirmationDiv.classList.add("hidden");
-}
+const cancelConfirmation = () => {
+  confirmationDiv.classList.add("hidden");
+};
 
-const deleted=()=>{
-    confirmationDiv.classList.add("hidden");
-    popup.classList.add("hidden");
-    clickBtn.innerHTML = "Deleted Successfully";
-    clickBtn.disabled=true
-    setTimeout(() => {
-        clickBtn.disabled = false;
-        clickBtn.innerHTML='Click Me'
-        
-        result=false
-    }, 1000);
-}
+const deleted = () => {
+  confirmationDiv.classList.add("hidden");
+  popup.classList.add("hidden");
+  clickBtn.innerHTML = "Deleted Successfully";
+  clickBtn.disabled = true;
+  setTimeout(() => {
+    clickBtn.disabled = false;
+    clickBtn.innerHTML = "Click Me";
+
+    result = false;
+  }, 1000);
+};
